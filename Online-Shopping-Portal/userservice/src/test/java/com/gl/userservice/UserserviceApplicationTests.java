4ec764aee45198e280a314ce458e8291412f365f
ziazeshan141 <ziazeshan141@outlook.com>
2026-08-06 00:18:32 +0300

    change the file again

--- Online-Shopping-Portal/userservice/src/test/java/com/gl/userservice/UserserviceApplicationTests.java
+++ Online-Shopping-Portal/userservice/src/test/java/com/gl/userservice/UserserviceApplicationTests.java
@@ -3,11 +3,24 @@ package com.gl.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:user_management;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.open-in-view=false",
+
+    "spring.sql.init.mode=never",
+
+    "spring.security.user.name=test-user",
+    "spring.security.user.password=test-password"
+})
 class UserserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
